e91fac52e3a211e731f0ba6fa4fdb00393f3157d
Marwan-Aly37 <143884401+Marwan-Aly37@users.noreply.github.com>
2024-08-28 13:19:52 +0300

    Add files via upload

--- src/turtle2/scripts/keyboard.py
+++ src/turtle2/scripts/keyboard.py
@@ -3,16 +3,28 @@ import rospy
 from geometry_msgs.msg import Twist
 import sys
 import select
+from std_msgs.msg import Int32
+class turtle_attributes:
+    def _init_ (self,pos_X,pos_y,health,atk_left):
+        self.pos_x = pos_X
+        self.pos_y = pos_y
+        self.health = health
+        self.atk_left = atk_left
+        health = 100
+        atk_left = 10
+def pose_callback(data):
+    global x ,y
+    x = data.x
+    y = data.y 
 
 def key_press():
     rospy.init_node('keyboard_control_node', anonymous=True)
     pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
+    pub_c = rospy.Publisher('chatter', Int32, queue_size=10)
     rate = rospy.Rate(10)  # 10 Hz
-
     while not rospy.is_shutdown():
         key = get_key()
         cmd = Twist()
-
         if key == 'w':
             cmd.linear.x = 1.0
         elif key == 's':
@@ -22,11 +34,9 @@ def key_press():
         elif key == 'd':
             cmd.angular.z = -1.0
         elif key == 'q':
-            cmd.linear.x = 0
-            cmd.angular.z = 0
+            pub_c.publish(1)
         else:
             continue
-
         pub.publish(cmd)
         rate.sleep()
 
