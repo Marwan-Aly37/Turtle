2ea726361502d73dd96d5e1affa3c8de84511dc8
Ibrahem Ahmed <ibrahem.ahmed6667@gmail.com>
2024-08-29 19:43:39 +0300

    Update keyboard3.py

--- src/turtle2/scripts/keyboard3.py
+++ src/turtle2/scripts/keyboard3.py
@@ -30,16 +30,16 @@ def key_press():
         cmd = Twist()
 
         if key == 'w':
-            cmd.linear.x = 1.0
+            cmd.linear.x = 1.75
         elif key == 's':
-            cmd.linear.x = -1.0
+            cmd.linear.x = -1.75
         elif key == 'a':
-            cmd.angular.z = 1.0
+            cmd.angular.z = 1.75
         elif key == 'd':
-            cmd.angular.z = -1.0
+            cmd.angular.z = -1.75
         elif key == 'q':
             current_time=time.time()
-            if (current_time - last_attack_time >= 3):
+            if (current_time - last_attack_time >= 1.5):
                 pub_tut_3.publish(1)
                 number_of_attacks -= 1
                 pub_tut_3_attacknum.publish(number_of_attacks)
