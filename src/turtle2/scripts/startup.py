568cd1ee2f954aec08cc423c77b65a3af13abfba
Marwan-Aly37 <143884401+Marwan-Aly37@users.noreply.github.com>
2024-08-29 16:06:48 +0300

    startup.py

--- src/turtle2/scripts/startup.py
+++ src/turtle2/scripts/startup.py
@@ -3,7 +3,7 @@ import subprocess
 import time
 subprocess.Popen(['rosrun','turtle2','window.py'])
 subprocess.Popen(['rosrun','turtle2','control.py'])
-time.sleep(0.7)
+time.sleep(1)
 subprocess.Popen(['rosrun','turtle2','node.py']).wait()
 
   
